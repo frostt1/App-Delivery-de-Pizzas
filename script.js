8b82c929a94086a16d82306b9e76097ae20232a4
frostt1 <caiohenriquedev@outlook.com>
2022-10-05 09:45:25 -0300

    adicionado modal e preenchimento de dados

--- script.js
+++ script.js
@@ -9,11 +9,25 @@ const selectAll = (element) => {
 pizzaJson.map((item, index) => {
     let pizzaItem = select(".models .pizza-item").cloneNode(true);
 
+    pizzaItem.setAttribute("data-key", index);
     pizzaItem.querySelector(".pizza-item--img img").src = item.img;
-
     pizzaItem.querySelector(".pizza-item--name").innerHTML = item.name;
     pizzaItem.querySelector(".pizza-item--price").innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector(".pizza-item--desc").innerHTML = item.description;
+    pizzaItem.querySelector("a").addEventListener("click", (e) => {
+        e.preventDefault();
+        let key = e.target.closest(".pizza-item").getAttribute("data-key");
+
+        select(".pizzaBig img").src = pizzaJson[key].img;
+        select(".pizzaInfo h1").innerHTML = pizzaJson[key].name;
+        select(".pizzaInfo--desc").innerHTML = pizzaJson[key].description;
+
+        select(".pizzaWindowArea").style.opacity = 0;
+        select(".pizzaWindowArea").style.display = "flex";
+        setTimeout(() => {
+            select(".pizzaWindowArea").style.opacity = 1;
+        }, 200);
+    });
 
     select(".pizza-area").append(pizzaItem);
 });
